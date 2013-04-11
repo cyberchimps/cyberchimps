9af008cd5c2a5f612c29d051e2ef123a33b69f75
marutim <marutim@mindfiresolutions.com>
2013-04-11 20:05:52 +0530

    Indentation done

--- search.php
+++ search.php
@@ -19,52 +19,52 @@ get_header(); ?>
 
 <div id="search_page" class="container-full-width">
 	
-<div class="container">
-	
-<div class="container-fluid">
-
-<?php do_action( 'cyberchimps_before_container'); ?>
-
-<div id="container" <?php cyberchimps_filter_container_class(); ?>>
-	
-	<?php do_action( 'cyberchimps_before_content_container'); ?>
-	
-	<div id="content" <?php cyberchimps_filter_content_class(); ?>>
+	<div class="container">
 		
-		<?php do_action( 'cyberchimps_before_content'); ?>
+		<div class="container-fluid">
 		
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'cyberchimps' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
-			</header>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'search' ); ?>
-
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'no-results', 'search' ); ?>
-
-		<?php endif; ?>
-	
-		<?php do_action( 'cyberchimps_after_content'); ?>
+			<?php do_action( 'cyberchimps_before_container'); ?>
+			
+			<div id="container" <?php cyberchimps_filter_container_class(); ?>>
+				
+				<?php do_action( 'cyberchimps_before_content_container'); ?>
+				
+				<div id="content" <?php cyberchimps_filter_content_class(); ?>>
+					
+					<?php do_action( 'cyberchimps_before_content'); ?>
+					
+					<?php if ( have_posts() ) : ?>
+			
+						<header class="page-header">
+							<h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'cyberchimps' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+						</header>
+			
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+			
+							<?php get_template_part( 'content', 'search' ); ?>
+			
+						<?php endwhile; ?>
+			
+					<?php else : ?>
+			
+						<?php get_template_part( 'no-results', 'search' ); ?>
+			
+					<?php endif; ?>
+				
+					<?php do_action( 'cyberchimps_after_content'); ?>
+					
+				</div><!-- #content -->
+				
+				<?php do_action( 'cyberchimps_after_content_container'); ?>
+					
+			</div><!-- #container .row-fluid-->
+			
+			<?php do_action( 'cyberchimps_after_container'); ?>
+			
+		</div><!--container fluid -->
 		
-	</div><!-- #content -->
-	
-	<?php do_action( 'cyberchimps_after_content_container'); ?>
-		
-</div><!-- #container .row-fluid-->
-
-<?php do_action( 'cyberchimps_after_container'); ?>
-
-</div><!--container fluid -->
-
-</div><!-- container -->
+	</div><!-- container -->
 
 </div><!-- container full width -->
 
