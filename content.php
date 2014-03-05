97f9d746918ca07ba88174631521ec349cb4cc93
Simon S <simon@cyberchimps.com>
2014-03-05 14:50:33 +0530

    Fixed conflict between blog post excerpt option and bbpress user profile page.

--- content.php
+++ content.php
@@ -107,7 +107,7 @@
 		</div><!-- .entry-summary -->
 
 	<?php
-	else : // blog post pages ?>
+	elseif( is_home() ) : // blog post pages ?>
 		<?php if( cyberchimps_get_option( 'post_excerpts', 0 ) ): ?>
 			<div class="entry-summary">
 				<?php cyberchimps_featured_image(); ?>
@@ -120,7 +120,11 @@
 				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'cyberchimps' ), 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
 		<?php endif; ?>
-
+		
+	<?php else: ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div><!-- .entry-content -->
 	<?php endif; ?>
 
 	<footer class="entry-meta">
